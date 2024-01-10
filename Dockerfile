59efcb34725db5cb8057c753bb82159b82eec423
oh1oiso <ugo.combet1@gmail.com>
2024-01-10 10:34:08 +0000

    Ajout des pages html

--- Dockerfile
+++ Dockerfile
@@ -7,14 +7,14 @@ COPY ./requirements.txt /app/requirements.txt
 # Repertoire de travail
 WORKDIR /app
 
+# Création et activation d'un environnement virtuel
+RUN python -m venv venv
+
 # Installation des dépendances
 RUN pip install -r requirements.txt
 
 # Copie le contenu du repertoire actuel dans /app du conteneur
 COPY ./app /app
-
-# Entrée par défaut lors de l'exécution du conteneur
-ENTRYPOINT ["python"]
 
 # Expose port 5000 for Flask to listen on
 EXPOSE 5000
@@ -22,7 +22,5 @@ EXPOSE 5000
 # Define environment variable for Flask
 ENV FLASK_APP=app.py
 
-
-
 # Démarrez l'application Flash
-CMD ["app.py"]
+CMD ["python", "app.py"]
--- app/app.py
+++ app/app.py
@@ -1,12 +1,33 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import os
-
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
+@app.route('/formulaire')
+def formulaire():
+    return render_template('form_page.html')
+
+@app.route('/inscription', methods=['POST'])
+def inscription():
+    nom = request.form.get('nom')
+    prenom = request.form.get('prenom')
+    login = request.form.get('login')
+    mot_de_passe = request.form.get('mot_de_passe')
+
+    # Process registration data (add to a database, etc.)
+    # For now, just print the data
+    print(f"Inscription réussie : Nom = {nom}, Prénom = {prenom}, Login = {login}")
+
+    return render_template('inscription_success.html', nom=nom, prenom=prenom)
+
+@app.route('/videotheque')
+def videotheque():
+    return render_template('videotheque.html')
+
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    print(f"Starting the app on port {port}")
+    app.run(debug=True, host='0.0.0.0', port=port, use_reloader=True)
